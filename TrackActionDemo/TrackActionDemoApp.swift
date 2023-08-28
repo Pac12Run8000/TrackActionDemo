98603804cc424600c9835d9bb2fcd2cd3f3ee790
Pac12Run8000 <gangstarr78992@>
2023-08-28 06:24:22 -0700

    Initial Commit

---
+++ TrackActionDemo/TrackActionDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrackActionDemoApp.swift
+//  TrackActionDemo
+//
+//  Created by Michelle Grover on 8/28/23.
+//
+
+import SwiftUI
+
+@main
+struct TrackActionDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
